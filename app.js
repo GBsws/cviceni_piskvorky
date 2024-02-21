7fce0b74b98963b7f28068f3141ca8ef4c928042
Gabriela Biswasová <pracho.dayat@seznam.cz>
2024-02-21 11:30:22 +0100

    finished

--- app.js
+++ app.js
@@ -15,45 +15,19 @@ hry.forEach((hra)=>{
 	const vitez= findWinner(zjednodusenaPolicka)
 	const vysledek = hra.querySelector('.vysledek')
 	if(vitez==='o'){
-		vysledek.textContent='vyhralo kolecko'
+		vysledek.textContent='Vyhrálo kolečko.'
 	}
 	if(vitez==='x'){
-		vysledek.textContent='vyhral krizek'
+		vysledek.textContent='Vyhrál křížek.'
 	}
 	if(vitez==='tie'){
-		vysledek.textContent='Nerozhodne'
+		vysledek.textContent='Nerozhodně.'
 	}
 	if(vitez==='o'){
-		vysledek.textContent='vyhralo kolecko'
+		vysledek.textContent='Vyhrálo kolečko.'
 	}
 	if(vitez===null){
-		vysledek.textContent='Hra jeste probiha'
+		vysledek.textContent='Hra ještě probíhá.'
 	}
 })
 
-// import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4' // Tento řádek vám umožňuje používat funkci findWinner od jiného programátora. Více se o zápisu s import dozvíte v pozdějších lekcích.
-
-// const hry = document.querySelectorAll('.hra')
-// hry.forEach((hra) => {
-// 	const policka = hra.querySelectorAll('.policko')
-// 	const zjednodusenaPolicka = Array.from(policka).map((policko) => {
-// 		if (policko.classList.contains('kolecko')) {
-// 			return 'o'
-// 		}
-// 		if (policko.classList.contains('krizek')) {
-// 			return 'x'
-// 		}
-// 		return '_'
-// 	})
-// 	const vitez = findWinner(zjednodusenaPolicka)
-// 	const vysledek = hra.querySelector('.vysledek')
-// 	if (vitez === 'o') {
-// 		vysledek.textContent = 'Vyhrálo kolečko!'
-// 	} else if (vitez === 'x') {
-// 		vysledek.textContent = 'Vyhrál křížek!'
-// 	} else if (vitez === 'tie') {
-// 		vysledek.textContent = 'Nerozhodně!'
-// 	} else if (vitez === null) {
-// 		vysledek.textContent = 'Hra ještě probíhá.'
-// 	}
-// })
